e5a68863187ac0dda9739025bcd82bcef99bd9cc
James Farrell <jamesfarrell@google.com>
2023-12-06 18:15:24 +0000

    Diff dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use std::collections::{BTreeMap, BTreeSet, HashSet};
 use std::path::Path;
 use std::process::{Command, Output};
 use std::str::from_utf8;
 
 use anyhow::{anyhow, Result};
 use clap::{Parser, Subcommand};
-use crates_index;
 use reqwest;
 use tempfile;
 
@@ -19,6 +19,7 @@ struct Cli {
 enum Cmd {
     Versions,
     Diff { v1: String, v2: String },
+    Deps { v1: String, v2: String },
 }
 
 fn success_or_error(cmd: &mut Command) -> Result<Output> {
@@ -82,6 +83,109 @@ fn main() -> Result<()> {
                 .output()?;
             println!("{}", from_utf8(&diff.stdout)?);
 
+            dir.close()?
+        }
+        Cmd::Deps { v1, v2 } => {
+            let index = crates_index::GitIndex::new_cargo_default()?;
+            let krate = index
+                .crate_(&args.crate_name)
+                .ok_or(anyhow!("Couldn't find crate name {}", args.crate_name))?;
+            let v1 = krate
+                .versions()
+                .iter()
+                .find(|v| v.version() == v1)
+                .cloned()
+                .ok_or(anyhow!(
+                    "Couldn't find version {} for crate {}",
+                    v1,
+                    args.crate_name
+                ))?;
+            let v2 = krate
+                .versions()
+                .iter()
+                .find(|v| v.version() == v2)
+                .cloned()
+                .ok_or(anyhow!(
+                    "Couldn't find version {} for crate {}",
+                    v2,
+                    args.crate_name
+                ))?;
+            let v1_map = v1
+                .dependencies()
+                .iter()
+                .map(|d| (d.name(), d))
+                .collect::<BTreeMap<_, _>>();
+            let v2_map = v2
+                .dependencies()
+                .iter()
+                .map(|d| (d.name(), d))
+                .collect::<BTreeMap<_, _>>();
+            let v1_deps = v1
+                .dependencies()
+                .iter()
+                .map(|d| d.name())
+                .collect::<BTreeSet<_>>();
+            let v2_deps = v2
+                .dependencies()
+                .iter()
+                .map(|d| d.name())
+                .collect::<BTreeSet<_>>();
+            let added = v2_deps.difference(&v1_deps).collect::<HashSet<_>>();
+            let removed = v1_deps.difference(&v2_deps).collect::<HashSet<_>>();
+
+            let dir = tempfile::tempdir()?;
+            for dep in v1_deps.union(&v2_deps) {
+                if added.contains(dep) {
+                    println!(
+                        "{}",
+                        format!("+{:#?}", v2_map.get(dep).unwrap())
+                            .lines()
+                            .collect::<Vec<_>>()
+                            .join("\n+")
+                    );
+                } else if removed.contains(dep) {
+                    println!(
+                        "{}",
+                        format!("-{:#?}", v1_map.get(dep).unwrap())
+                            .lines()
+                            .collect::<Vec<_>>()
+                            .join("\n-")
+                    );
+                } else {
+                    std::fs::write(
+                        dir.as_ref()
+                            .join(format!("{}-{}", &args.crate_name, v1.version())),
+                        format!("{:#?}\n", v1_map.get(dep).unwrap()),
+                    )?;
+                    std::fs::write(
+                        dir.as_ref()
+                            .join(format!("{}-{}", &args.crate_name, v2.version())),
+                        format!("{:#?}\n", v2_map.get(dep).unwrap()),
+                    )?;
+                    let diff = Command::new("diff")
+                        .args([
+                            "-w",
+                            // "--color=always",
+                            "--unified=1000",
+                        ])
+                        .args([
+                            format!("{}-{}", args.crate_name, v1.version()),
+                            format!("{}-{}", args.crate_name, v2.version()),
+                        ])
+                        .current_dir(&dir)
+                        .output()?;
+                    if !diff.status.success() {
+                        println!(
+                            "{}",
+                            from_utf8(&diff.stdout)?
+                                .lines()
+                                .skip(3)
+                                .collect::<Vec<_>>()
+                                .join("\n")
+                        );
+                    }
+                }
+            }
             dir.close()?
         }
     }
